1b7678d531c77304b2c8097c26692033ca32e0c0
Anton Babenko <anton@antonbabenko.com>
2019-11-08 22:03:23 +0100

    Updated simple example a bit

--- examples/notify-slack-simple/main.tf
+++ examples/notify-slack-simple/main.tf
@@ -2,10 +2,15 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+resource "aws_sns_topic" "my_sns" {
+  name = "my-sns"
+}
+
 module "notify_slack" {
   source = "../../"
 
-  sns_topic_name = "slack-topic"
+  sns_topic_name   = aws_sns_topic.my_sns.name
+  create_sns_topic = false
 
   slack_webhook_url = "https://hooks.slack.com/services/AAA/BBB/CCC"
   slack_channel     = "aws-notification"
