b574873f01140d9df4fd9403afccd34675115a9b
Adrian Maceiras <amac425@gmail.com>
2019-11-13 01:12:02 -0600

    merge dictionaries

--- functions/notify_slack.py
+++ functions/notify_slack.py
@@ -68,9 +68,8 @@ def notify_slack(subject, message, region):
         notification = cloudwatch_notification(message, region)
         payload['text'] = "AWS CloudWatch notification - " + message["AlarmName"]
         payload['attachments'].append(notification)
-    elif "attachments" in message:
-        payload['text'] = message.get('text', 'AWS notification')
-        payload['attachments'] = message['attachments']
+    elif "attachments" in message or "text" in message:
+        payload = {**payload, **message}
     else:
         payload['text'] = "AWS notification"
         payload['attachments'].append(default_notification(subject, message))
