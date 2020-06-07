37f4b07b65d364d1e62540d902b345c610567307
Anton Babenko <anton@antonbabenko.com>
2020-06-07 17:31:32 +0200

    feat: Rewrote module to handle Lambda resources properly with terraform-aws-lambda module (#83)

--- outputs.tf
+++ outputs.tf
@@ -5,37 +5,37 @@ output "this_slack_topic_arn" {
 
 output "lambda_iam_role_arn" {
   description = "The ARN of the IAM role used by Lambda function"
-  value       = element(concat(aws_iam_role.lambda.*.arn, [""]), 0)
+  value       = module.lambda.lambda_role_arn
 }
 
 output "lambda_iam_role_name" {
   description = "The name of the IAM role used by Lambda function"
-  value       = element(concat(aws_iam_role.lambda.*.name, [""]), 0)
+  value       = module.lambda.lambda_role_name
 }
 
 output "notify_slack_lambda_function_arn" {
   description = "The ARN of the Lambda function"
-  value       = element(concat(aws_lambda_function.notify_slack.*.arn, [""]), 0)
+  value       = module.lambda.this_lambda_function_arn
 }
 
 output "notify_slack_lambda_function_name" {
   description = "The name of the Lambda function"
-  value       = element(concat(aws_lambda_function.notify_slack.*.function_name, [""]), 0)
+  value       = module.lambda.this_lambda_function_name
 }
 
 output "notify_slack_lambda_function_invoke_arn" {
   description = "The ARN to be used for invoking Lambda function from API Gateway"
-  value       = element(concat(aws_lambda_function.notify_slack.*.invoke_arn, [""]), 0)
+  value       = module.lambda.this_lambda_function_invoke_arn
 }
 
 output "notify_slack_lambda_function_last_modified" {
   description = "The date Lambda function was last modified"
-  value       = element(concat(aws_lambda_function.notify_slack.*.last_modified, [""]), 0)
+  value       = module.lambda.this_lambda_function_last_modified
 }
 
 output "notify_slack_lambda_function_version" {
   description = "Latest published version of your Lambda function"
-  value       = element(concat(aws_lambda_function.notify_slack.*.version, [""]), 0)
+  value       = module.lambda.this_lambda_function_version
 }
 
 output "lambda_cloudwatch_log_group_arn" {
