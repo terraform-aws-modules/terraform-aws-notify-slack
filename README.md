ff034caeb1d1a12a3122f7cd7f6341092cfc8cb3
Alden Quimby <aldenquimby@gmail.com>
2024-09-19 23:23:02 -0400

    fix: give SNS feedback access to CloudWatch

--- README.md
+++ README.md
@@ -89,6 +89,7 @@ See the [functions](https://github.com/terraform-aws-modules/terraform-aws-notif
 | [aws_caller_identity.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity) | data source |
 | [aws_iam_policy_document.lambda](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
 | [aws_iam_policy_document.sns_feedback](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
+| [aws_iam_policy_document.sns_feedback_allow_log_creation](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
 | [aws_partition.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/partition) | data source |
 | [aws_region.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/region) | data source |
 
--- iam.tf
+++ iam.tf
@@ -21,6 +21,22 @@ data "aws_iam_policy_document" "sns_feedback" {
   }
 }
 
+data "aws_iam_policy_document" "sns_feedback_allow_log_creation" {
+  count = local.create_sns_feedback_role ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:PutMetricFilter",
+      "logs:PutRetentionPolicy",
+    ]
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_role" "sns_feedback_role" {
   count = local.create_sns_feedback_role ? 1 : 0
 
@@ -30,6 +46,11 @@ resource "aws_iam_role" "sns_feedback_role" {
   force_detach_policies = var.sns_topic_feedback_role_force_detach_policies
   permissions_boundary  = var.sns_topic_feedback_role_permissions_boundary
   assume_role_policy    = data.aws_iam_policy_document.sns_feedback[0].json
+
+  inline_policy {
+    name   = "allow-log-creation"
+    policy = data.aws_iam_policy_document.sns_feedback_allow_log_creation[0].json
+  }
 
   tags = merge(
     var.tags,
